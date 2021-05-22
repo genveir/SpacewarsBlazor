1a12beb7274302a644a768f80fc53468beb2756c
Geerten Vink <maedaros@yahoo.com>
2021-05-22 09:28:40 +0200

    fixed that ships just never log off anymore

---
+++ SpacewarsBlazor/DisposableComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpacewarsBlazor
+{
+    public class DisposableComponent : ComponentBase, IDisposable
+    {
+        public virtual void Dispose()
+        {
+            
+        }
+    }
+}
